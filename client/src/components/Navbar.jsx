73c03459c45b10b890013e7aa4fb902caa73a0ef
ANNE_DAMIEN <PRO.DAMIENANNE@gmail.com>
2024-04-29 17:06:47 +0200

    fix asset

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import "../assets/images/logo300.png"
+
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
+import logo300 from "../assets/images/logo300.png"
 import "./LandingPage.scss";
 
 function Navbar() {
